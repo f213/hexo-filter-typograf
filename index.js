8319770d9b40ade6b151bf006972103792f8e638
Fedor Borshev <f@f213.in>
2015-05-14 08:28:15 +0300

    ₽ support

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 var Typograf = require('typograf'),
     tp = new Typograf({lang: hexo.config.language, mode: 'name'});
 
+if(hexo.config.language == 'ru'){
+    tp.enable('ru/money/ruble');
+}
+
 hexo.extend.filter.register('after_post_render', function(data) {
     data.content = tp.execute(data.content);
     return data;
