cbcc8fd5eb063b8812d6c17e13ec0290d00311ce
Fedor Borshev <f@f213.in>
2015-09-03 23:55:10 +0300

    Now typograf rules can be configured via _config.yml

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 This a [Hexo](https://hexo.io/) plugin for running all your posts through the [Typograf](https://github.com/typograf/typograf).
 
-No tests or config variables.
+Config example:
+```
+typograf:
+    enable:
+        - ru/money/ruble
+    disable:
+        - ru/optalign
+        - ru/nbsp/but
+```
+Full rule names are described [here](https://github.com/typograf/typograf/blob/dev/docs/RULES.ru.md).
--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 'use strict';
 
+var _ = require('lodash');
+
 var Typograf = require('typograf'),
     tp = new Typograf({lang: hexo.config.language, mode: 'name'});
 
-if(hexo.config.language == 'ru'){
-    tp.enable('ru/money/ruble');
-    tp.disable('ru/optalign');
-}
+var config = {
+    typograf: {
+        enable: [],
+        disable: [],
+    }
+};
+
+hexo.config = _.merge(hexo.config, config);
+
+hexo.config.typograf.enable.forEach(function(rule){
+    tp.enable(rule);
+});
+hexo.config.typograf.disable.forEach(function(rule){
+    tp.disable(rule);
+});
 
 hexo.extend.filter.register('after_post_render', function(data) {
     if(data.path.match(/\.(css|js)$/)){
