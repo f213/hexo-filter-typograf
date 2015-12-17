7a9b7deb1c71df8d9447fe8c7607eaac35cda9d8
Fedor Borshev <f@f213.in>
2015-12-17 21:57:03 +0300

    typograf → richtypo.js

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
 # hexo-filter-typograf
 
-This a [Hexo](https://hexo.io/) plugin for running all your posts through the [Typograf](https://github.com/typograf/typograf).
+This a [Hexo](https://hexo.io/) plugin for running all your posts through the [Richtypo.js](https://github.com/sapegin/richtypo.js). Very usefull for those Russians who love typography. No configurable variables, just install it and use.
 
-Config example:
-```
-typograf:
-    enable:
-        - ru/money/ruble
-    disable:
-        - ru/optalign
-        - ru/nbsp/but
-```
-Full rule names are described [here](https://github.com/typograf/typograf/blob/dev/docs/RULES.ru.md).
+Richtypo.js is available only for russian and english languages.
--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 'use strict';
 
-var _ = require('lodash');
-
-var Typograf = require('typograf'),
-    tp = new Typograf({lang: hexo.config.language, mode: 'name'});
-
-var config = {
-    typograf: {
-        enable: [],
-        disable: [],
-    }
-};
-
-hexo.config = _.merge(hexo.config, config);
-
-hexo.config.typograf.enable.forEach(function(rule){
-    tp.enable(rule);
-});
-hexo.config.typograf.disable.forEach(function(rule){
-    tp.disable(rule);
-});
+var richtypo = require('richtypo');
 
 hexo.extend.filter.register('after_post_render', function(data) {
     if(data.path.match(/\.(css|js)$/)){
         return data;
     }
-    data.content = tp.execute(data.content);
+    data.content = richtypo.rich(data.content, hexo.config.language);
+
     return data;
 });
