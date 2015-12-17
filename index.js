f53123b543cafd4829d13e9b9d0d77a2a369c3d7
Fedor Borshev <f@f213.in>
2015-12-17 22:23:09 +0300

    More safe rules

--- index.js
+++ index.js
@@ -2,11 +2,20 @@
 
 var richtypo = require('richtypo');
 
+var richtypoRules = [
+    'save_tags',
+    'cleanup_before',
+    'spaces_lite',
+    'spaces',
+    'cleanup_after',
+    'restore_tags'
+];
+
 hexo.extend.filter.register('after_post_render', function(data) {
     if(data.path.match(/\.(css|js)$/)){
         return data;
     }
-    data.content = richtypo.rich(data.content, hexo.config.language);
+    data.content = richtypo.richtypo(data.content, richtypoRules, hexo.config.language);
 
     return data;
 });
